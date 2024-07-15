81ade265ab9050d3808ae770c33368a2ef770a98
jakubmanczak <jakub@manczak.net>
2024-07-15 20:36:48 +0200

    [12] set up logging via tracing crate

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,30 @@
 use axum::Router;
 use tokio::net::TcpListener;
+use tracing::error;
 mod routes;
 mod setup;
 
 #[tokio::main]
 async fn main() {
+    setup::initialise_logging();
+
     let app = Router::new().merge(routes::routes());
 
     let addr = setup::get_socket_addr();
     let listener = match TcpListener::bind(&addr).await {
         Ok(listener) => listener,
         Err(e) => {
-            panic!("error creating a listener ({e})");
+            error!("Error creating a listener: {e}");
+            panic!();
         }
     };
+    setup::report_listener_socket_addr(&listener);
 
     match axum::serve(listener, app).await {
         Ok(..) => (),
         Err(e) => {
-            panic!("could not serve ({e})");
+            error!("Error serving app on listener: {e}");
+            panic!();
         }
     };
 }
--- src/setup.rs
+++ src/setup.rs
@@ -1,4 +1,27 @@
 use std::net::{Ipv4Addr, SocketAddrV4};
+use tokio::net::TcpListener;
+use tracing::{error, info, Level};
+use tracing_subscriber::FmtSubscriber;
+
+pub fn initialise_logging() {
+    let subscriber = FmtSubscriber::builder()
+        .with_max_level(Level::TRACE)
+        .finish();
+    tracing::subscriber::set_global_default(subscriber)
+        .expect("setting default tracing subscriber failed!");
+    info!("Response cannon spinning up...");
+}
+
+pub fn report_listener_socket_addr(listener: &TcpListener) {
+    let addr = match listener.local_addr() {
+        Ok(addr) => addr,
+        Err(e) => {
+            error!("Error getting listener socket address: {e}");
+            panic!();
+        }
+    };
+    info!("Listener socket address is: {}", addr.to_string());
+}
 
 fn get_env_port() -> u16 {
     let portstr = match std::env::var("PORT") {
@@ -9,7 +32,8 @@ fn get_env_port() -> u16 {
     return match portstr.parse() {
         Ok(num) => num,
         Err(e) => {
-            panic!("Could not parse env var PORT! ({e})");
+            error!("Error parsing PORT environment variable: {e}");
+            panic!();
         }
     };
 }
