4e50636b1d9a9f937b1479e957d489bf3ecbbc8c
jakubmanczak <jakub@manczak.net>
2024-10-18 21:06:13 +0200

    [18/19] tournmUser, tournmAdmin, profpic optional
    * move roles to a TournamentUser struct wrapping the User struct
    * provide a mechanism for checking whether user is infrastucture admin
    (such a user is uniquely represented with a nil but valid ulid)
    * make profile picture optional

--- src/users/mod.rs
+++ src/users/mod.rs
@@ -11,11 +11,29 @@ pub struct User {
     pub id: Ulid,
     pub username: String,
     pub displayname: String,
-    pub profilepicture: PhotoUrl,
+    pub profilepicture: Option<PhotoUrl>,
+}
+
+pub struct TournamentUser {
+    pub user: User,
     pub roles: Vec<Role>,
 }
 
 impl User {
+    pub fn is_infrastructure_admin(&self) -> bool {
+        self.id.is_nil()
+    }
+    pub fn new_infrastructure_admin() -> Self {
+        User {
+            id: Ulid::nil(),
+            username: String::from("admin"),
+            displayname: String::from("admin"),
+            profilepicture: None,
+        }
+    }
+}
+
+impl TournamentUser {
     pub fn has_permission(&self, permission: Permission) -> bool {
         self.roles
             .iter()
@@ -24,13 +42,21 @@ impl User {
 }
 
 #[test]
-fn construct_user() {
-    let admin = User {
-        id: Ulid::new(),
-        username: String::from("admin"),
-        displayname: String::from("admin"),
-        profilepicture: PhotoUrl::new("https://i.imgur.com/hbrb2U0.png").unwrap(),
-        roles: vec![Role::Admin, Role::Organizer, Role::Judge, Role::Marshall],
+fn construct_tournament_user() {
+    let org = TournamentUser {
+        user: User {
+            id: Ulid::new(),
+            username: String::from("org"),
+            displayname: String::from("organizator"),
+            profilepicture: Some(PhotoUrl::new("https://i.imgur.com/hbrb2U0.png").unwrap()),
+        },
+        roles: vec![Role::Organizer, Role::Judge, Role::Marshall],
     };
-    admin.has_permission(Permission::DeleteUsers);
+    assert!(org.has_permission(Permission::DeleteUsers));
+}
+
+#[test]
+fn construct_infradmin() {
+    let infradmin = User::new_infrastructure_admin();
+    assert!(infradmin.is_infrastructure_admin());
 }
