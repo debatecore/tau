be845439532b2af6818e50434c70931e424deeb3
jakubmanczak <jakub@manczak.net>
2024-12-20 21:38:33 +0100

    [22] database schema amendments
    mmhehehehe yess
    
    *sips tea from hongkong*

--- migrations/20241219_init.sql
+++ migrations/20241219_init.sql
@@ -1,64 +1,57 @@
 CREATE TABLE IF NOT EXISTS users (
-    id              TEXT NOT NULL UNIQUE PRIMARY KEY,
-    handle          TEXT NOT NULL,
-    passwordHash   TEXT NOT NULL,
-    pictureLink    TEXT
+    id              UUID NOT NULL UNIQUE PRIMARY KEY,
+    handle          TEXT NOT NULL UNIQUE,
+    passwordHash    TEXT NOT NULL,
+    pictureLink     TEXT DEFAULT NULL
 );
 
 CREATE TABLE IF NOT EXISTS motions (
-    id           TEXT NOT NULL UNIQUE PRIMARY KEY,
-    motion       TEXT NOT NULL,
-    adinfo       TEXT
+    id           UUID NOT NULL UNIQUE PRIMARY KEY,
+    motion       TEXT NOT NULL UNIQUE,
+    adinfo       TEXT DEFAULT NULL
 );
 
 CREATE TABLE IF NOT EXISTS debates (
-    id               TEXT NOT NULL UNIQUE,
-    team1Id          TEXT NOT NULL,
-    team2Id          TEXT NOT NULL,
-    motionId         TEXT NOT NULL REFERENCES motions(id),
-    marshallUserId   TEXT NOT NULL REFERENCES users(id),
-    propositionTeamAssignment    INTEGER NOT NULL
+    id               UUID NOT NULL UNIQUE,
+    team1Id          UUID NOT NULL REFERENCES teams(id),
+    team2Id          UUID NOT NULL REFERENCES teams(id),
+    motionId         UUID NOT NULL REFERENCES motions(id),
+    marshallUserId   UUID NOT NULL REFERENCES users(id),
+    propositionTeamAssignment INTEGER NOT NULL DEFAULT 0
     -- 0 ^^ if undecided, otherwise 1 or 2
 );
 
-CREATE TABLE IF NOT EXISTS debate_judge (
-    id              TEXT NOT NULL UNIQUE PRIMARY KEY,
-    judgeUserId     TEXT NOT NULL REFERENCES users(id),
-    debateId        TEXT NOT NULL REFERENCES debates(id)
+CREATE TABLE IF NOT EXISTS debate_judge_assignment (
+    id              UUID NOT NULL UNIQUE PRIMARY KEY,
+    judgeUserId     UUID NOT NULL REFERENCES users(id),
+    debateId        UUID NOT NULL REFERENCES debates(id)
 );
 
 CREATE TABLE IF NOT EXISTS tournaments (
-    id              TEXT NOT NULL UNIQUE PRIMARY KEY,
-    fullName        TEXT NOT NULL,
+    id              UUID NOT NULL UNIQUE PRIMARY KEY,
+    fullName        TEXT NOT NULL UNIQUE,
     shortenedName   TEXT NOT NULL
 );
 
-
 CREATE TABLE IF NOT EXISTS roles (
-    id              TEXT NOT NULL UNIQUE PRIMARY KEY,
-    userId          TEXT NOT NULL REFERENCES users(id),
-    tournamentId    TEXT NOT NULL REFERENCES tournaments(id),
-    roles           TEXT[] NOT NULL
+    id              UUID NOT NULL UNIQUE PRIMARY KEY,
+    userId          UUID NOT NULL REFERENCES users(id),
+    tournamentId    UUID NOT NULL REFERENCES tournaments(id),
+    roles           TEXT[] DEFAULT NULL
 );
 
 CREATE TABLE IF NOT EXISTS teams (
-    id              TEXT NOT NULL UNIQUE PRIMARY KEY,
+    id              UUID NOT NULL UNIQUE PRIMARY KEY,
     fullName        TEXT NOT NULL,
     shortenedName   TEXT NOT NULL,
-    tournamentId    TEXT NOT NULL REFERENCES tournaments(id)
+    tournamentId    UUID NOT NULL REFERENCES tournaments(id)
 );
 
-CREATE TABLE IF NOT EXISTS speakers (
-    id                 TEXT NOT NULL UNIQUE PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS attendees (
+    id                 UUID NOT NULL UNIQUE PRIMARY KEY,
     name               TEXT NOT NULL,
-    position           INTEGER NOT NULL,
-    teamId             TEXT NOT NULL REFERENCES teams(id),
-    individualPoints   INTEGER NOT NULL,
-    penaltyPoints      INTEGER NOT NULL
+    position           INTEGER DEFAULT NULL,
+    teamId             UUID DEFAULT NULL REFERENCES teams(id),
+    individualPoints   INTEGER NOT NULL DEFAULT 0,
+    penaltyPoints      INTEGER NOT NULL DEFAULT 0
 );
-
-CREATE TABLE IF NOT EXISTS tournament_user (
-    id              TEXT NOT NULL UNIQUE PRIMARY KEY,
-    userId          TEXT NOT NULL REFERENCES users(id),
-    tournamentId    TEXT NOT NULL REFERENCES tournaments(id)
-)
