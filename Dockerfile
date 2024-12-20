575c8e52cda46cc91d7012c7630f8501a62cc446
jakubmanczak <jakub@manczak.net>
2024-12-20 16:36:03 +0100

    [28] mount build.rs, don't env bad/empty git hash

--- Dockerfile
+++ Dockerfile
@@ -19,6 +19,7 @@ RUN apk add --no-cache clang lld musl-dev git
 RUN --mount=type=bind,source=src,target=src \
     --mount=type=bind,source=Cargo.toml,target=Cargo.toml \
     --mount=type=bind,source=Cargo.lock,target=Cargo.lock \
+    --mount=type=bind,source=build.rs,target=build.rs \
     --mount=type=cache,target=/app/target/ \
     --mount=type=cache,target=/usr/local/cargo/git/db \
     --mount=type=cache,target=/usr/local/cargo/registry/ \
--- build.rs
+++ build.rs
@@ -1,10 +1,15 @@
 use std::process::Command;
 
 fn main() {
-    println!("cargo::rustc-env=GIT_COMMIT_HASH={}", "");
+    println!("cargo::rustc-env=GIT_COMMIT_HASH=UNKNOWN!");
     match Command::new("git").args(&["rev-parse", "HEAD"]).output() {
         Ok(output) => match String::from_utf8(output.stdout) {
-            Ok(hash) => println!("cargo::rustc-env=GIT_COMMIT_HASH={}", hash),
+            Ok(hash) => {
+                if hash.is_empty() || hash.starts_with("fatal") {
+                    return;
+                }
+                println!("cargo::rustc-env=GIT_COMMIT_HASH={}", hash)
+            }
             Err(_) => (),
         },
         Err(_) => (),
