067cadb775d068d8ef651a05fc4a452c286b0c66
Jakub Mańczak <jakub@manczak.net>
2024-12-29 14:31:28 +0100

    Update src/users/auth/mod.rs
    Co-authored-by: Mateusz Dobrzyński <60257096+Mateusz-Dobrzynski@users.noreply.github.com>

--- src/users/auth/mod.rs
+++ src/users/auth/mod.rs
@@ -6,6 +6,6 @@ pub mod error;
 pub mod session;
 pub mod userimpl;
 
-pub const AUTH_SESSION_COOKIE_NAME: &str = "tausession";
+pub const AUTH_SESSION_COOKIE_NAME: &str = "tau_session";
 pub const AUTH_SESSION_LENGTH: Duration = Duration::weeks(1);
 pub const AUTH_SESSION_COOKIE_LENGTH: CookieDuration = CookieDuration::weeks(1);
