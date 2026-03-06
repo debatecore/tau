e1811a49ffab1b8780e87f6a78ee9cefb9bdefd8
Mateusz Dobrzyński <mateusz.dobrzynski64@gmail.com>
2026-03-06 18:43:09 +0100

    [47] test all endpoints for roles management

--- tests/common/roles_utils.rs
+++ tests/common/roles_utils.rs
@@ -2,7 +2,7 @@ use reqwest::{Client, Response};
 use tau::setup::get_socket_addr;
 use tau::tournament::roles::Role;
 
-pub async fn grant_roles(
+pub async fn create_roles(
     user_id: &str,
     tournament_id: &str,
     roles: Vec<Role>,
@@ -18,6 +18,64 @@ pub async fn grant_roles(
             socket_address, user_id, tournament_id
         ))
         .body(roles_string)
+        .header("accept", "text/plain")
+        .header("Content-Type", "application/json")
+        .bearer_auth(token)
+        .send()
+        .await
+        .unwrap()
+}
+
+pub async fn get_roles(user_id: &str, tournament_id: &str, token: &str) -> Response {
+    let socket_address = get_socket_addr();
+    let client = Client::new();
+
+    client
+        .get(format!(
+            "http://{}/user/{}/tournament/{}/roles",
+            socket_address, user_id, tournament_id
+        ))
+        .header("accept", "text/plain")
+        .header("Content-Type", "application/json")
+        .bearer_auth(token)
+        .send()
+        .await
+        .unwrap()
+}
+
+pub async fn patch_roles(
+    user_id: &str,
+    tournament_id: &str,
+    roles: Vec<Role>,
+    token: &str,
+) -> Response {
+    let socket_address = get_socket_addr();
+    let client = Client::new();
+    let roles_string: String = serde_json::to_string(&roles).unwrap();
+
+    client
+        .patch(format!(
+            "http://{}/user/{}/tournament/{}/roles",
+            socket_address, user_id, tournament_id
+        ))
+        .body(roles_string)
+        .header("accept", "text/plain")
+        .header("Content-Type", "application/json")
+        .bearer_auth(token)
+        .send()
+        .await
+        .unwrap()
+}
+
+pub async fn delete_roles(user_id: &str, tournament_id: &str, token: &str) -> Response {
+    let socket_address = get_socket_addr();
+    let client = Client::new();
+
+    client
+        .delete(format!(
+            "http://{}/user/{}/tournament/{}/roles",
+            socket_address, user_id, tournament_id
+        ))
         .header("accept", "text/plain")
         .header("Content-Type", "application/json")
         .bearer_auth(token)
