8be127a8ad820e5c1c7bdf7a5f76e14d0bee0b84
Arsan <155844600+arsansomuncu@users.noreply.github.com>
2026-03-23 23:21:31 +0100

    Enhance permissions struct with serialization traits
    Added Serialize and Deserialize traits to the permissions struct.

--- src/users/permissions.rs
+++ src/users/permissions.rs
@@ -1,7 +1,9 @@
-use strum::{EnumIter, VariantArray};
+use strum::{EnumIter, VariantArray, EnumString}; // Added EnumString
 use utoipa::ToSchema;
+use serde::{Deserialize, Serialize} // use serde :: {Deserialize, Serialize} added.
 
-#[derive(Debug, VariantArray, EnumIter, Clone, PartialEq, ToSchema)]
+#[derive(Debug, VariantArray, EnumIter, Clone, PartialEq, ToSchema, Serialize, Deserialize)]
+#[strum(serialize_all = "PascalCase")] // Ensures parsing matches the variant names
 /// To perform any operation, a user is required to have a corresponding
 /// permission. Permissions are predefined for each role.
 /// The infrastructure admin has all permissions and is allowed to perform
