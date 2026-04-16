7bbc44cf4c80d7e9ea8ba87fab09d9e0ae9df4e2
arsansomuncu <arsan.somuncu@gmail.com>
2026-04-16 20:47:08 +0200

    fix: typo is corrected on plans_tests.rs file

--- tests/integration_tests/plans_tests.rs
+++ tests/integration_tests/plans_tests.rs
@@ -102,7 +102,7 @@ async fn organizers_should_be_able_to_create_tournament_plan() -> Result<(), Omn
     ).await;
 
     let status = response.status();
-    let body = response.text().await.unwrap();
+    let _body = response.text().await.unwrap();
 
     assert_eq!(status, StatusCode::OK);
 
