89ba54e8ed96d80cb7d99d73cad2d532f7586187
arsansomuncu <arsan.somuncu@gmail.com>
2026-04-20 15:13:05 +0200

    fix: match structure changed with if-let structure on build.rs file

--- build.rs
+++ build.rs
@@ -7,7 +7,7 @@ fn main() {
     println!("cargo:rerun-if-changed=.git/HEAD");
 
     println!("cargo::rustc-env=GIT_COMMIT_HASH=UNKNOWN!");
-    if let Ok(output) = Command::new("git").args(&["rev-parse", "HEAD"]).output() {
+    if let Ok(output) = Command::new("git").args(["rev-parse", "HEAD"]).output() {
         if let Ok(hash) = String::from_utf8(output.stdout) {
             if !hash.is_empty() && !hash.starts_with("fatal") {
                 println!("cargo::rustc-env=GIT_COMMIT_HASH={}", hash);
@@ -17,7 +17,7 @@ fn main() {
 
     println!("cargo::rustc-env=GIT_STATUS_PORCELAIN=UNKNOWN!");
     if let Ok(output) = Command::new("git")
-        .args(&["status", "--porcelain"])
+        .args(["status", "--porcelain"])
         .output()
     {
         if let Ok(status) = String::from_utf8(output.stdout) {
