25d9d04147fbbd22090a3865c9481ce5df950bf7
arsansomuncu <arsan.somuncu@gmail.com>
2026-04-20 10:42:27 +0200

    TEST: Add file with bad formatting to verify CI check fails

---
+++ test_bad_formatting.rs
@@ -0,0 +1,10 @@
+// This file has intentionally bad formatting
+fn   test_function(  )  {
+    let x=1;
+    let y   =     2;
+    println!("{}",x+y);
+}
+
+pub fn  another_function(   a: i32,b:i32  ) -> i32 {
+    a+b
+}
